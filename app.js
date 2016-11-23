c42769eb88eac0a118f8fb310508283f7d068188
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-11-23 12:43:47 -0800

    api endpoints saved locally, removes server crash

--- app.js
+++ app.js
@@ -17,26 +17,6 @@ app.use(express.static(__dirname + '/public'));
 // load the routes
 require('./app/routes')(app);
 
-//api endpoints
-app.get('/api', controllers.api.index);
-
-app.get('/api/users', controllers.users.index);
-app.get('/api/users/:username', controllers.users.show);
-app.post('/api/users', controllers.users.create);
-app.delete('/api/users/:username', controllers.users.destroy);
-app.put('/api/users/:usersname', controllers.users.update);
-
-app.get('/api/exercises', controllers.exercises.index);
-app.get('/api/exercises/:exerciseId', controllers.exercises.show);
-app.post('/api/exercises', controllers.exercises.create);
-app.delete('/api/exercises/:exerciseId', controllers.exercises.destroy);
-app.put('/api/exercises/:exerciseId', controllers.exercises.update);
-
-app.get('/api/exercises/:exerciseId/comments', controllers.exerciseComments.index);
-app.post('/api/exercises/:exerciseId/comments', controllers.exerciseComments.create);
-app.delete('/api/exercises/:exerciseId/comments/:commentId', controllers.exerciseComments.destroy);
-app.put('/api/exercises/:exerciseId/comments/:commentId', controllers.exerciseComments.update);
-
 //server listening
 app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is running on http://localhost:3000/');
